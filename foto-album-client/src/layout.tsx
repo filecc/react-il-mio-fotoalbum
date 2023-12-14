7f6ad1d6d90cd2d198195807c19f0baf36c80c07
filecc <fil.gia@live.it>
2023-12-14 18:13:40 +0100

    Add classNames utility function to Layout component

--- foto-album-client/src/layout.tsx
+++ foto-album-client/src/layout.tsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react";
 import Navbar from "./components/Navbar";
 
-
+function classNames (...classes: string[]) {
+    return classes.filter(Boolean).join(' ')
+}
 export default function Layout({ children }: { children: React.ReactNode }) {
     const darkModePreference = window.matchMedia("(prefers-color-scheme: dark)").matches;
     const [darkModeActive, setDarkModeActive] = useState(darkModePreference);
@@ -16,7 +18,10 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     })
 
   return (
-    <main className={`${darkModeActive ? 'dark text-foreground bg-background' : ''}`}>
+    <main className={classNames(
+        darkModeActive ? 'dark text-foreground bg-background' : '',
+        'min-h-[100dvh] w-full flex flex-col items-center'
+    )}>
       <Navbar />
       {children}
     </main>
